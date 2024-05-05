34ff494e854a7e43c9d624a21e50a5619c3c8cac
techiemanish <manishtiwari627@gmail.com>
2024-05-05 17:47:58 +0530

    Contact us connected with backend, health check added in footer

--- src/App.js
+++ src/App.js
@@ -6,10 +6,34 @@ import Login from './login/Login';
 import Register from './login/Register';
 import Heading from './header/Heading';
 import AdminDashboard from './login/admin/AdminDashboard';
+import { Toaster } from 'react-hot-toast';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [status, setstatus] = useState({
+    css:"flex w-3 h-3 bg-red-500 rounded-full float-right mt-1 md:float-right md:mt-1",
+    tooltip: "Server is down"
+  });
+
+  useEffect(()=>{
+    axios.get("https://sankalpyouthfoundation-api.onrender.com/")
+    .then((res)=>{
+    if(res.status === 200){
+        setstatus({
+          css: "flex w-3 h-3 bg-green-500 rounded-full float-right mt-1 md:float-right md:mt-1",
+          tooltip: "Server is up"
+      });
+    }
+  })
+  }, []) 
+  
   return (
   <>
+  <div><Toaster
+      position="top-center"
+      reverseOrder={false}
+    /></div>
     <Heading heading="Sankalp Youth Foundation"/>
     <Routes>
       <Route path= "*" element={<Home/>}/>
@@ -18,7 +42,7 @@ function App() {
       <Route path= "/register" element={<Register/>}/>
       <Route path= "/admin" element={<AdminDashboard/>}/>
     </Routes>
-    <Footer/>
+    <Footer webstat = {status}/>
   </>
   );
 }
