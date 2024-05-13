28bb1715fff1d0e5f0a862d7687025a69e6d7ece
techiemanish <manishtiwari627@gmail.com>
2024-05-13 09:53:07 +0530

    Redux state fix for different windows

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
 import rootReducer from './rootReducer';
 
-const store = configureStore({ 
-  reducer: rootReducer  
+const serializedState = localStorage.getItem('syfreduxState');
+const preloadedState = serializedState ? JSON.parse(serializedState) : {};
+
+const store = configureStore({
+  reducer: rootReducer,
+  preloadedState: preloadedState
+});
+
+store.subscribe(() => {
+  const state = store.getState();
+  localStorage.setItem('syfreduxState', JSON.stringify(state));
 });
 
 export default store;
