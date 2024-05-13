dee8592e245e3a8f8abc5ce8fc5fc283ae341972
techiemanish <manishtiwari627@gmail.com>
2024-05-13 09:31:57 +0530

    Register message added for login UI

--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -84,9 +84,9 @@ function Login(props) {
             <Link to={"/login"} className="ml-auto text-sm text-blue-700 hover:underline dark:text-blue-500">Forgot Password?</Link>
         </div> */}
         <button type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login to your account</button>
-        {/* <div className="text-sm font-medium text-gray-500">
-            Not registered? <Link to={"/register"} className="text-blue-700 hover:underline dark:text-blue-500">Create account</Link>
-        </div> */}
+        <div className="text-sm font-medium text-gray-500">
+            To register, please contact the admin.
+        </div>
     </form>
 </div>
 </>
